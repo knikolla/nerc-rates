edbd5dc98695a5dbb06060e59f1b529114da0da4
Lars Kellogg-Stedman <lars@redhat.com>
2024-05-17 15:10:31 -0400

    Add validation for overlapping date ranges
    Ensure that date ranges in a `history` list do not overlap.

--- src/nerc_rates/tests/test_rates.py
+++ src/nerc_rates/tests/test_rates.py
@@ -23,6 +23,48 @@ def test_invalid_date_order():
         models.RateValue.model_validate(rate)
 
 
+@pytest.mark.parametrize(
+    "rate",
+    [
+        # Two values with no end date
+        {
+            "name": "Test Rate",
+            "history": [
+                {"value": "1", "from": "2020-01"},
+                {"value": "2", "from": "2020-03"},
+            ],
+        },
+        # Second value overlaps first value at end
+        {
+            "name": "Test Rate",
+            "history": [
+                {"value": "1", "from": "2020-01", "until": "2020-04"},
+                {"value": "2", "from": "2020-03"},
+            ],
+        },
+        # Second value overlaps first value at start
+        {
+            "name": "Test Rate",
+            "history": [
+                {"value": "1", "from": "2020-04", "until": "2020-06"},
+                {"value": "2", "from": "2020-03", "until": "2020-05"},
+            ],
+        },
+        # Second value is contained by first value
+        {
+            "name": "Test Rate",
+            "history": [
+                {"value": "1", "from": "2020-01", "until": "2020-06"},
+                {"value": "2", "from": "2020-03", "until": "2020-05"},
+            ],
+        },
+    ],
+)
+def test_invalid_date_overlap(rate):
+    with pytest.raises(ValueError):
+        models.RateItem.model_validate(rate)
+
+
 def test_rates_get_value_at():
     r = rates.Rates(
         [
